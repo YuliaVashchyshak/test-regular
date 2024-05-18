1b30dcd8c2a0895613ad48cdd40cb853801e1fb5
Yulia Vashchyshak <143491298+YuliaVashchyshak@users.noreply.github.com>
2024-05-18 20:26:40 +0300

    add hero section

--- wp-content/themes/theme/blocks/hero/block.php
+++ wp-content/themes/theme/blocks/hero/block.php
@@ -1,3 +1,12 @@
-<div class="wrapper">
-    <h1><?php the_field('title') ?></h1>
-</div>
+<section class="hero" style="background-image:url(<?php the_field('background') ?>">
+    <div class="hero__container container">
+        <div class="hero__wrapper">
+            <h1 class="hero__tile"><?php the_field('title') ?></h1>
+            <h2 class="hero__subtitle"><?php the_field('subtitle') ?></h2>
+            <?php $link = get_field('link') ?>
+            <a href="<?php echo $link['url'] ?>" class="hero__link button"><?php echo $link['title'] ?></a>
+        </div>
+    </div>
+    <img src="<?php the_field('image') ?>" alt="man" class="hero__image">
+
+</section>
